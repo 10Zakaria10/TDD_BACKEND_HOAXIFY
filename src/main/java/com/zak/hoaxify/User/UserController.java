393351b793009fce5538a243309531498d208d01
Zakaria Lachguar <zakaria.lachgar@adria-bt.com>
2020-10-11 23:08:13 +0100

    add crossOrigin

--- src/main/java/com/zak/hoaxify/User/UserController.java
+++ src/main/java/com/zak/hoaxify/User/UserController.java
@@ -2,11 +2,13 @@ package com.zak.hoaxify.User;
 
 import com.zak.hoaxify.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
